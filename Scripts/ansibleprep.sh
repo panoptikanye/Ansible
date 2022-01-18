d3b4785c4e5d20d57c4608500f1459094b443a51
panoptikanye <67125631+panoptikanye@users.noreply.github.com>
2022-01-18 16:20:11 -0700

    Update ansibleprep.sh

--- Scripts/ansibleprep.sh
+++ Scripts/ansibleprep.sh
@@ -4,9 +4,12 @@
 ## Remember to chmod +x
 
 # clear known hosts
-> ~/.ssh/known_hosts
+#> ~/.ssh/known_hosts
+
+# create /.ssh
+mkdir ~/.ssh
 
 # create new key without password
 ssh-keygen -C "fedora" -t ed25519 -f ~/.ssh/id_ed25519 -q -N ""
 
-# 
+ 

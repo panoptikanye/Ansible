38255590c20f18f3281bde04491e2ec1cc7ed88b
panoptikanye <67125631+panoptikanye@users.noreply.github.com>
2022-01-18 16:16:22 -0700

    setting up ssh keys

---
+++ Scripts/ansibleprep.sh
@@ -0,0 +1,12 @@
+#!
+
+## Prep fresh installs for Ansible
+## Remember to chmod +x
+
+# clear known hosts
+> ~/.ssh/known_hosts
+
+# create new key without password
+ssh-keygen -C "fedora" -t ed25519 -f ~/.ssh/id_ed25519 -q -N ""
+
+# 

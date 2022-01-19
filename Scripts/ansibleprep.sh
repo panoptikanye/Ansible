634a6e645187708fa08d89c1cae77e50f2ec2f42
panoptikanye <67125631+panoptikanye@users.noreply.github.com>
2022-01-18 17:30:52 -0700

    exchange identities

--- Scripts/ansibleprep.sh
+++ Scripts/ansibleprep.sh
@@ -11,5 +11,5 @@
 
 # create new key without password
 ssh-keygen -C "fedora" -t ed25519 -f ~/.ssh/id_ed25519 -q -N ""
-
- 
+### !!! REMEMBER TO COPY IDENTITY OF EACH NEW NODE TO ANSIBLE CONTROLLER, THEN COPY ANSIBLE CONTROLLER IDENTITIES TO EACH NEW NODE !!!
+### --- ex: ssh-copy-id -i ~/.ssh/id_ed25519.pub [USERNAME]@xxx.xxx.xxx.xxx
